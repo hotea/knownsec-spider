7055184d538f473e701f9004f13c6090544e0e84
kai <okkylesu@gmail.com>
2016-04-06 23:52:01 +0800

    finished

--- crawler.py
+++ crawler.py
@@ -16,6 +16,7 @@ import requests
 import sqlite3
 import sys
 import threading
+import traceback
 import time
 
 
@@ -31,30 +32,32 @@ class Crawler():
         self.lock = lock  # 一把坚硬的锁
         self.pages = set()  # 集合用于网址去重
         self.db = db  # 数据库实例
-        self.table_name = '_' + re.search(r'^(?:https?://)(?:www.)?([^/]*)', url).groups()[-1].replace('.', '_')  # 以起始网址命名数据表, 如"_sina_com_cn"
+        self.table_name = '_' + re.search(r'^(?:https?://)(?:www.)?([^/]*)', url).groups(
+        )[-1].replace('.', '_')  # 以起始网址命名数据表, 如"_sina_com_cn"
         self.db.create_table(self.table_name)
-        self.cur_level = 1  # 当前所处的层
-        self.cur_total = 1  # 当前层的URL总数
+        self.current_level = 1  # 当前所处的层
+        self.current_total = 1  # 当前层的URL总数
         self.next_total = 0  # 下层的URL总数
-        self.cur_failed = 0  # 当前层访问失败的URL总数
+        self.current_failed = 0  # 当前层访问失败的URL总数
         self.total = 0  # 所有层的URL总数, 用于最后打印报告
-        self.total_failed = 0 # 所有层失败URL的总数
-        self.total_finished = 0 # 所有层完成URL的总数, 在正常退出而非用户中断的情况下, 此数据等于 self.total
+        self.total_failed = 0  # 所有层失败URL的总数
+        self.total_finished = 0  # 所有层完成URL的总数, 在正常退出而非用户中断的情况下, 此数据等于 self.total
 
     def crawl(self, url, depth):
         '''实现爬取功能的主要函数'''
 
         if depth > self.depth_limit:  # 当前深度超过指定深度, 停止抓取, 将工作队列标记为任务完成
             self.db.conn.commit()
-            self.logger.info('save content to database table: {}'.format(self.table_name))
-            self.tp.jobs.task_done() # 通知队列任务完成
-        if depth > self.cur_level:  # 进入下一层
+            self.logger.debug(
+                'save content to database table: {}'.format(self.table_name))
+            self.tp.jobs.task_done()  # 通知队列任务完成
+        if depth > self.current_level:  # 进入下一层
             # 当切换层数时, 用sleep来简化同步, 较早到达下层的线程等待一段时间,
             # 使所有线程都抵达下一层, 然后再更新下层任务数, 保证数目正确
-            time.sleep(1 * self.cur_level * self.tp.thread_num)
+            time.sleep(1 * self.current_level * self.tp.thread_num)
             with self.lock:
-                if depth > self.cur_level:  # 双重检查锁, 保证该语句块只被多个线程中的一个执行一次
-                    self.change_level(depth) # 切换层数
+                if depth > self.current_level:  # 双重检查锁, 保证该语句块只被多个线程中的一个执行一次
+                    self.change_level(depth)  # 切换层数
             return
         try:
             if url not in self.pages:
@@ -62,33 +65,34 @@ class Crawler():
                 self.logger.debug('fetch url: {}'.format(url))
                 new_urls = self.get_urls_from_url(url)  # 获取此页面链出的URL
                 if not new_urls:
-                    self.cur_failed += 1
+                    self.current_failed += 1
                     return
                 if depth == self.depth_limit:  # 如果该层是最后一层则不需要添加新任务, 直接返回
                     return
                 self.next_total += len(new_urls)  # 更新下层任务总数
-                for new_url in new_urls:  # 将每一个新解析出的URL加入任务 
+                for new_url in new_urls:  # 将每一个新解析出的URL加入任务
                     self.tp.add_job(self.crawl, new_url, depth + 1)
         except Exception as e:
             self.logger.critical(e)
+            self.logger.critical(traceback.format_exc())
             raise e
 
     def change_level(self, depth):
         '''进入下一层'''
         self.logger.info('level {} finished, total URL number: {}, failed {}'.format(
-            self.cur_level, self.cur_total, self.cur_failed))
+            self.current_level, self.current_total, self.current_failed))
         print('\n' + '=' * 60)
-        self.total_finished += self.tp.cur_finished # 更新完成总数
-        self.tp.cur_finished = 0  # 当前层已完成任务数重置为0
-        self.total_failed += self.cur_failed # 更新失败总数
-        self.cur_failed = 0 # 当前层失败数置0
-        self.total += self.cur_total # 更新URL总数
-        self.cur_total = self.next_total  # 设置新一层的任务总数, 此数目由爬取完上层URL之后得来
-        self.next_total = 0 # 下层总数置0
-        self.cur_level = depth # 当前层编号设置为下层编号
-        self.logger.info('enter the {} level'.format(self.cur_level))
+        self.total_finished += self.tp.current_finished  # 更新完成总数
+        self.tp.current_finished = 0  # 当前层已完成任务数重置为0
+        self.total_failed += self.current_failed  # 更新失败总数
+        self.current_failed = 0  # 当前层失败数置0
+        self.total += self.current_total  # 更新URL总数
+        self.current_total = self.next_total  # 设置新一层的任务总数, 此数目由爬取完上层URL之后得来
+        self.next_total = 0  # 下层总数置0
+        self.current_level = depth  # 当前层编号设置为下层编号
+        self.logger.info('enter the {} level'.format(self.current_level))
         self.logger.info('the URL numbers of the {} level is {}'.format(
-            self.cur_level, self.cur_total))
+            self.current_level, self.current_total))
 
     def get_urls_from_url(self, url):
         '''获取某页面的所有url '''
@@ -99,7 +103,8 @@ class Crawler():
             'Connection': 'keep-alive',
         }
         try:
-            response = requests.get(url, headers=headers, timeout=2) # 可视网络情况调整 timeout 时长
+            # 可视网络情况调整 timeout 时长
+            response = requests.get(url, headers=headers, timeout=2)
         except requests.Timeout as e:
             self.logger.error(e)
             return
@@ -147,20 +152,23 @@ class Crawler():
             self.db.curs.execute("INSERT INTO {} (url, key, content) VALUES (:url, :key, :content)".format(self.table_name),
                                  {"url": url, "key": self.key, "content": content})
             self.db.conn.commit()
-            self.logger.info(
+            self.logger.debug(
                 'save content to database table: {}'.format(self.table_name))
         except sqlite3.Error as e:
             self.logger.critical('sqlite3 error: {}'.format(e))
+            self.logger.critical(traceback.format_exc())
         finally:
             self.lock.release()
-    
+
     def report(self):
         '''最后的统计报告'''
         time.sleep(5)
-        print('\n','-' * 30,' 结果统计 ', '-' * 30)
-        print('总URL数: {}, 共抓取: {}, 其中失败: {}'.format(self.total, self.total_finished, self.total_failed))
+        print('\n', '-' * 30, ' 结果统计 ', '-' * 30)
+        print('总URL数: {}, 共抓取: {}, 其中失败: {}'.format(
+            self.total, self.total_finished, self.total_failed))
         try:
-            num = self.db.curs.execute("SELECT COUNT(*) AS NUM FROM {}".format(self.table_name))
+            num = self.db.curs.execute(
+                "SELECT COUNT(*) AS NUM FROM {}".format(self.table_name))
             print('共保存了 {} 个页面'.format(num.fetchone()[0]))
             self.db.conn.close()
         except sqlite3.Error as e:
@@ -170,12 +178,13 @@ class Crawler():
 
 class Database():
     '''数据库类'''
+
     def __init__(self, dbfile, logger):
         self.dbfile = dbfile  # 指定数据库文件
         self.logger = logger
-        self.init_database()
+        self._init_database()
 
-    def init_database(self):
+    def _init_database(self):
         try:
             self.conn = sqlite3.connect(self.dbfile, check_same_thread=False)
             self.logger.info(
@@ -183,11 +192,12 @@ class Database():
             self.curs = self.conn.cursor()
         except sqlite3.Connection.Error as e:
             self.logger.critical('sqlite3 error: {}'.format(e))
+            self.logger.critical(traceback.format_exc())
             return
         except sqlite3.Error as e:
             self.logger.critical('sqlite3 error: {}'.format(e))
+            self.logger.critical(traceback.format_exc())
             return
-
 
     def create_table(self, table):
         '''创建数据表'''
@@ -220,8 +230,8 @@ class Progress(threading.Thread):
         sys.stdout.write(' ' * 60 + '\r')
         sys.stdout.flush()
         sys.stdout.write('当前层: {}, 该层URL总数: {}, 已完成: {}, 失败: {}, 进度: {:.2f}%\r'.format(
-            self.c.cur_level, self.c.cur_total, self.tp.cur_finished,
-            self.c.cur_failed, self.tp.cur_finished / self.c.cur_total * 100))
+            self.c.current_level, self.c.current_total, self.tp.current_finished,
+            self.c.current_failed, self.tp.current_finished / self.c.current_total * 100))
         sys.stdout.flush()
 
 
@@ -238,7 +248,7 @@ class Worker(threading.Thread):
         while True:
             func, args, kargs = self.tp.jobs.get()  # 获取一个任务
             func(*args, **kargs)  # 执行该任务
-            self.tp.cur_finished += 1  # 更新完成任务数
+            self.tp.current_finished += 1  # 更新完成任务数
             self.tp.jobs.task_done()  # 通知任务队列该任务完成
 
 
@@ -246,11 +256,11 @@ class Threadpool():
     '''线程池'''
 
     def __init__(self, thread_num):
-        self.thread_num = thread_num # 线程池大小
+        self.thread_num = thread_num  # 线程池大小
         self.jobs = queue.Queue()  # 工作队列
         self.threads = []  # 线程池
-        self.cur_finished = 0 # 当前层完成任务数初始化为0
-        self.__init_threadpool(thread_num) # 初始化线程池
+        self.current_finished = 0  # 当前层完成任务数初始化为0
+        self.__init_threadpool(thread_num)  # 初始化线程池
 
     def __init_threadpool(self, thread_num):
         for i in range(thread_num):
@@ -332,7 +342,7 @@ def main():
     (1, '互联网', 'http://www.sina.com.cn')
     '''
     lock = threading.RLock()  # 线程锁
-    opt = get_options() # 获取命令行选项及参数
+    opt = get_options()  # 获取命令行选项及参数
     if opt.testself:
         import doctest
         print(doctest.testmod())
@@ -340,26 +350,26 @@ def main():
     if not opt.url:
         sys.exit('Need to specify the URL, use "-h" see help\n')
     if not opt.url.startswith('http://'):
-        opt.url = 'http://' + opt.url 
+        opt.url = 'http://' + opt.url
     logger = get_a_logger(opt.logfile, opt.loglevel)
-    db = Database(opt.dbfile, logger) # 数据库实例
-    tp = Threadpool(opt.thread) # 线程池实例
-    c = Crawler(opt.url, opt.depth, db, opt.key, logger, tp, lock) # 爬虫实例
-    progress = Progress(c, tp) # 进度实例
+    db = Database(opt.dbfile, logger)  # 数据库实例
+    tp = Threadpool(opt.thread)  # 线程池实例
+    c = Crawler(opt.url, opt.depth, db, opt.key, logger, tp, lock)  # 爬虫实例
+    progress = Progress(c, tp)  # 进度实例
     tp.add_job(c.crawl, opt.url, 1)  # 将起始URL加入任务队列, 设置当前深度为1
     logger.info('tasks start!, destination url: {}, depth limitation: {}'.format(
         opt.url, opt.depth))
     try:
         tp.wait_completion()  # 等待所有任务完成
-        c.total += c.cur_total # 任务完成后更新完成总数, 用于报告
-        c.total_failed += c.cur_failed
-        c.total_finished += tp.cur_finished
+        c.total += c.current_total  # 任务完成后更新完成总数, 用于报告
+        c.total_failed += c.current_failed
+        c.total_finished += tp.current_finished
         logger.info('all tasks done')
     except KeyboardInterrupt:  # 按 Ctrl+C 退出
         logger.info('task canceled by user')
-        c.total += c.cur_total
-        c.total_failed += c.cur_failed
-        c.total_finished += tp.cur_finished
+        c.total += c.current_total
+        c.total_failed += c.current_failed
+        c.total_finished += tp.current_finished
         c.report()
         sys.exit('\n任务取消, 退出... \n')
     logger.info('exit normally')
